a92890356cbcfc25746779d9f43e6a0891de01e3
Mohamed <zayanm410@gmail.com>
2020-07-17 21:32:32 +0200

    dispose

--- core/src/com/engine/animation/AnimationGenerator.java
+++ core/src/com/engine/animation/AnimationGenerator.java
@@ -2,6 +2,7 @@ package com.engine.animation;
 
 import java.util.HashMap;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -11,15 +12,24 @@ import com.engine.exception.OverwriteException;
 // AnimationGenerator : handle only horizontal sprite sheets
 public class AnimationGenerator {
 
-	static protected HashMap<String, HashMap<String, Animation<TextureRegion>>> animationRepo = 
-										new HashMap<String, HashMap<String, Animation<TextureRegion>>>();
+	static protected HashMap<String, HashMap<String, Animation<TextureRegion>>> animationRepo = new HashMap<String, HashMap<String, Animation<TextureRegion>>>();
 
-	public AnimationGenerator() {
+	static protected float stateTime;
 
+	protected SpriteBatch spriteBatch;
+
+	protected Texture spriteSheet;
+
+	public AnimationGenerator(String path) {
+
+		spriteBatch = new SpriteBatch();
+		spriteSheet = new Texture(path);
+
+		stateTime = 0.0f;
 	}
 
-	static public void addAnimation(String name, String path, int nFrame, float frameDuration, int[] startKeys,
-			int[] endKeys, String[] typeKeys) throws OverwriteException {
+	public void addAnimation(String name, int nFrame, float frameDuration, int[] startKeys, int[] endKeys,
+			String[] typeKeys) throws OverwriteException {
 
 		/**
 		 * Create animation, of given sprite sheet, then put it inside the animation
@@ -45,9 +55,8 @@ public class AnimationGenerator {
 			throw new OverwriteException("Name :" + name + " already exist in the animation repository.");
 		} else {
 
-			HashMap<String, Animation<TextureRegion>> animator = new HashMap<String, Animation<TextureRegion>>();
-
-			Texture spriteSheet = new Texture(path);
+			HashMap<String, Animation<TextureRegion>> animator = 
+										new HashMap<String, Animation<TextureRegion>>();
 
 			int pixelStep = spriteSheet.getWidth() / nFrame;
 			int tileHeight = spriteSheet.getHeight();
@@ -85,15 +94,33 @@ public class AnimationGenerator {
 		}
 	}
 
-	static public void animate(String name, String mode, float stateTime, float x, float y) {
+	public void animate(String name, String mode, float x, float y) {
 
-		SpriteBatch spriteBatch = new SpriteBatch();
-
+		stateTime += Gdx.graphics.getDeltaTime();
 		TextureRegion currentFrame = animationRepo.get(name).get(mode).getKeyFrame(stateTime, true);
 
 		spriteBatch.begin();
 		spriteBatch.draw(currentFrame, x, y);
 		spriteBatch.end();
+	}
+
+	/**
+	 * Dispose all TiledMaps and OrthogonalTiledMapRenderer objects located in the
+	 * maps repository then clear the repository
+	 * 
+	 * @param id     : String - The id which used to access specific map.
+	 * @param envCam : OrthographicCamera - The camera used to render the TiledMap
+	 *               with given id.
+	 */
+	public void dispose() {
+
+		spriteSheet.dispose();
+		spriteBatch.dispose();
+	}
+
+	static public void disposeAll() {
+
+		animationRepo.clear();
 	}
 
 }
--- core/src/com/hercules/game/GameAdapter.java
+++ core/src/com/hercules/game/GameAdapter.java
@@ -18,78 +18,77 @@ public class GameAdapter extends ApplicationAdapter {
 	public static final int SCALE = 2; // Center Scale
 
 	public static final float GU = 100.0f; // Box2D Game-Unit Scaler
-	public static final float FPR = 1 / 60f; // Frame per second (Delta Time)
+	public static final float FPS = 1 / 60f; // Frame per second (Delta Time)
 
 	// Test MapGenerator
 	public static final String MAP_DIR = "./epic-scene/map/";
 	public static final String[] MAP = { "level-1.tmx" };
 	public static int initial_zoom_level = 1500;
-	
+
 	OrthographicCamera envCam;
 	OrthographicCamera playerCam;
 
 	// Test AnimationGenerator
+	AnimationGenerator demon;
+
 	public static final String name = "demon";
 	public static final String path = "./sprite-sheets/demon.png";
 	public static final float frameDuration = 0.09f;
 	public static final int nFrame = 21;
-	
-	public static final int []startKeys = {0, 3, 9, 13, 15};
-	public static final int []endKeys = {2, 8, 12, 14, 20};
-	public static final String []typeKeys = {"idle", "walk", "attack", "hurt", "death"};
-	
-	// init-render 
+
+	public static final int[] startKeys = { 0, 3, 9, 13, 15 };
+	public static final int[] endKeys = { 2, 8, 12, 14, 20 };
+	public static final String[] typeKeys = { "idle", "walk", "attack", "hurt", "death" };
+
+	// init-render
 	public float stateTime = 0.0f;
 	public float x_postion = 32.0f; // player - x
 	public float y_postion = 32.0f; // player - y
 	public String mode = "idle";
-	
+
 	@Override
 	public void create() {
 
 		// Test MapGenerator
 		MapGenerator.setMap(MAP_DIR, MAP[0], 0);
-		
+
 		// Test AnimationGenerator
+
+		demon = new AnimationGenerator(path); // enemy
 		try {
-			AnimationGenerator.addAnimation(name, path, nFrame, frameDuration, 
-												startKeys, endKeys, typeKeys);
+			demon.addAnimation(name, nFrame, frameDuration, startKeys, endKeys, typeKeys);
 		} catch (OverwriteException error) {
 			error.printStackTrace();
 		}
-		
+
 		float x = MapGenerator.getLayerWidth("0", 0);
 		float y = MapGenerator.getLayerHeight("0", 0);
 
 		envCam = new OrthographicCamera(V_WIDTH * SCALE, V_HEIGHT * SCALE);
 		envCam.setToOrtho(false, x - initial_zoom_level, y * 2.25f - initial_zoom_level);
-		
-		System.out.println(envCam.position.x);
-//		playerCam = new OrthographicCamera(100, 100); 
-//		playerCam.setToOrtho(false, x - initial_zoom_level, y * 2.25f - initial_zoom_level);
-		
+
 	}
 
 	@Override
 	public void render() {
 
-//		/////////////// Handle Inputs //////////////
+		/////////////// Handle Inputs //////////////
 		if (Gdx.input.isKeyJustPressed(Keys.ESCAPE)) {
 			Gdx.app.exit();
 		}
-		if(Gdx.input.isKeyPressed(Keys.RIGHT)){
-		    x_postion +=3;
-		    mode = "walk";
-		}
-		if(Gdx.input.isKeyPressed(Keys.LEFT)){
-			x_postion -=3;
+		if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			x_postion += 3;
 			mode = "walk";
 		}
-		
-		if(Gdx.input.isKeyPressed(Keys.SPACE)){
+		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+			x_postion -= 3;
+			mode = "walk";
+		}
+
+		if (Gdx.input.isKeyPressed(Keys.SPACE)) {
 			mode = "attack";
 		}
-		
+
 		///////////// Clear Screen /////////////////////////
 		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen
@@ -105,17 +104,19 @@ public class GameAdapter extends ApplicationAdapter {
 		}
 
 		///////////// Update /////////////////////
-		
-		stateTime += Gdx.graphics.getDeltaTime();
-		AnimationGenerator.animate(name, mode, stateTime, x_postion, y_postion);
-		
+
+		demon.animate(name, mode, x_postion, y_postion);
+
 		// re-init
 		mode = "idle";
 	}
 
 	@Override
 	public void dispose() {
+		
 		MapGenerator.disposeAll();
+		demon.dispose();
+		AnimationGenerator.disposeAll();
 	}
 
 }
